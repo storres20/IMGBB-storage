1d6d526f93e5606761a7324ac044071f70546079
storres20 <sanchotorres777@gmail.com>
2022-04-24 04:17:59 -0500

    ADD convert image to base64

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+import FileBase from 'react-file-base64';
+
+import { useState } from 'react';
+
 function App() {
+  const [listingData, setListingData] = useState("");
+  
+  let data64 //init variable
+  
+  //console.log(listingData)
+  
+  /* Put this Conditional for starting web app o refreshing
+   * to avoid errors because initial value is ""
+   * initial value:
+   * listingData = "" and listingData.selectedFile = undefined
+  */
+  
+  if (listingData !== "") {
+    data64 = listingData.selectedFile.split(',')
+    console.log(data64); //data64 = image converted to base64
+  }
+  
+  //Send to ImgBB with AXIOS
+  const handleClick = () => {
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React File to Base64 Converter</h1>
+      
+      <div>
+      <FileBase type="file" multiple={false} onDone={({base64}) => setListingData({ ...listingData, selectedFile: base64})}/>
+      <button type='button' onClick={() => handleClick()}>Send</button>
+      </div>
+      
+      {/* show selected image */}
+      <img src={data64} alt="" style={{ width: 350 }} />
+      
+      
     </div>
   );
 }
